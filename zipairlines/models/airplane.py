2000100d2bbd206331464dd546934f2bddaf7dff
shadowwa1k3r <ergash1994@gmail.com>
2022-04-30 17:22:05 +0500

    AirplaneModel

--- zipairlines/models/airplane.py
+++ zipairlines/models/airplane.py
@@ -1,11 +1,25 @@
 from django.db import models
+from math import log10
 
 class Airplane(models.Model):
     airplane_id = models.IntegerField()
     passenger_count = models.IntegerField()
+    fuel_tank = models.IntegerField(null=True, required=False)
 
     class Meta:
         db_table = 'airplane'
     
     def __str__(self):
         return 'airplane ' + str(self.airplane_id)
+
+    def fuel_consumption_per_minute(self):
+        """
+        calculate fuel consumption
+        """
+        return log10(self.airplane_id*0.80)+self.passenger_count*0.002
+
+    def save(self, *args, **kwargs):
+        self.fuel_tank = 200*self.airplane_id
+        super(Airplane, self).save(*args, **kwargs)
+        
+        
